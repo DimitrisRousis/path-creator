c2200326755e2deaab7c0c4945dd07a02f648d80
DESKTOP-JTDMC90\Δημήτρης <dimitrisrousis@yahoo.gr>
2018-05-02 19:38:59 +0300

    modules'  declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { AgmCoreModule } from '@agm/core';
+import { ActionsComponent } from './actions/actions.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionsComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDZNCKjxrCKcKqrDH967QsjDghLtACTHNU'}),     
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    FormsModule  
+    ],    
   providers: [],
   bootstrap: [AppComponent]
 })
